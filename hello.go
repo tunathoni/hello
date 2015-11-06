e5562dd0121c86a32578a13243d320c786c78cf2
Mochamad Fathoni <tunathoni@gmail.com>
2015-11-06 16:15:44 +0700

    First Learn Go

---
+++ hello.go
@@ -0,0 +1,14 @@
+package main 
+
+import "fmt"
+
+func main() {
+
+	var firstName string = "Mochamad"
+	var lastName string
+
+	lastName = "Fathoni"
+
+	fmt.Printf("My name is : %s %s \n", firstName, lastName)
+
+}
